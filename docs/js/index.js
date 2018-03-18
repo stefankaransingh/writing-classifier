4fada14a5b06e7a3ded5988e3bde06c20e1bf4de
stefankaransingh <stefankaransingh@gmail.com>
2018-03-18 10:32:00 +0000

    Update index.html

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,81 +1,83 @@
-(function() {
-	var canvas = document.querySelector('#canvas');
-	var context = canvas.getContext('2d');
-	canvas.width = 280;
-	canvas.height = 280;
+jQuery(document).ready(function($) {
+	(function() {
+		var canvas = document.querySelector('#canvas');
+		var context = canvas.getContext('2d');
+		canvas.width = 280;
+		canvas.height = 280;
 
-	var Mouse = { x: 0, y: 0 };
-	var lastMouse = { x: 0, y: 0 };
-	context.fillStyle = 'white';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	context.color = 'black';
-	context.lineWidth = 10;
-	context.lineJoin = context.lineCap = 'round';
+		var Mouse = { x: 0, y: 0 };
+		var lastMouse = { x: 0, y: 0 };
+		context.fillStyle = 'white';
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		context.color = 'black';
+		context.lineWidth = 10;
+		context.lineJoin = context.lineCap = 'round';
 
-	debug();
+		debug();
 
-	canvas.addEventListener(
-		'mousemove',
-		function(e) {
-			lastMouse.x = Mouse.x;
-			lastMouse.y = Mouse.y;
+		canvas.addEventListener(
+			'mousemove',
+			function(e) {
+				lastMouse.x = Mouse.x;
+				lastMouse.y = Mouse.y;
 
-			Mouse.x = e.pageX - this.offsetLeft;
-			Mouse.y = e.pageY - this.offsetTop;
-		},
-		false
-	);
+				Mouse.x = e.pageX - this.offsetLeft;
+				Mouse.y = e.pageY - this.offsetTop;
+			},
+			false
+		);
 
-	canvas.addEventListener(
-		'mousedown',
-		function(e) {
-			canvas.addEventListener('mousemove', onPaint, false);
-		},
-		false
-	);
+		canvas.addEventListener(
+			'mousedown',
+			function(e) {
+				canvas.addEventListener('mousemove', onPaint, false);
+			},
+			false
+		);
 
-	canvas.addEventListener(
-		'mouseup',
-		function() {
-			canvas.removeEventListener('mousemove', onPaint, false);
-		},
-		false
-	);
+		canvas.addEventListener(
+			'mouseup',
+			function() {
+				canvas.removeEventListener('mousemove', onPaint, false);
+			},
+			false
+		);
 
-	var onPaint = function() {
-		context.lineWidth = context.lineWidth;
-		context.lineJoin = 'round';
-		context.lineCap = 'round';
-		context.strokeStyle = context.color;
+		var onPaint = function() {
+			context.lineWidth = context.lineWidth;
+			context.lineJoin = 'round';
+			context.lineCap = 'round';
+			context.strokeStyle = context.color;
 
-		context.beginPath();
-		context.moveTo(lastMouse.x, lastMouse.y);
-		context.lineTo(Mouse.x, Mouse.y);
-		context.closePath();
-		context.stroke();
-	};
+			context.beginPath();
+			context.moveTo(lastMouse.x, lastMouse.y);
+			context.lineTo(Mouse.x, Mouse.y);
+			context.closePath();
+			context.stroke();
+		};
 
-	function debug() {
-		/* CLEAR BUTTON */
-		var clearButton = $('#clearButton');
+		function debug() {
+			/* CLEAR BUTTON */
+			var clearButton = $('#clearButton');
 
-		clearButton.on('click', function() {
-			context.clearRect(0, 0, 280, 280);
-			context.fillStyle = 'white';
-			context.fillRect(0, 0, canvas.width, canvas.height);
-		});
+			clearButton.on('click', function() {
+				context.clearRect(0, 0, 280, 280);
+				context.fillStyle = 'white';
+				context.fillRect(0, 0, canvas.width, canvas.height);
+			});
 
-		/* COLOR SELECTOR */
+			/* COLOR SELECTOR */
 
-		$('#colors').change(function() {
-			var color = $('#colors').val();
-			context.color = color;
-		});
+			$('#colors').change(function() {
+				var color = $('#colors').val();
+				context.color = color;
+			});
 
-		/* LINE WIDTH */
+			/* LINE WIDTH */
 
-		$('#lineWidth').change(function() {
-			context.lineWidth = $(this).val();
-		});
-	}
-})();
+			$('#lineWidth').change(function() {
+				context.lineWidth = $(this).val();
+			});
+		}
+	})();
+});
